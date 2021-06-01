2d76071819cdc35926afb75b773e6a51feca0056
thiagous06 <64616891+thiagous06@users.noreply.github.com>
2021-06-01 15:06:44 -0300

    Update install.sh
    Arreglo de errores

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@ mkdir -p tools/Wordlist
 mkdir -p tools/BruteForce
 mkdir -p tools/Reconocimiento
 mkdir -p tools/Spoofing
-pip3 install py-getch 
-pip3 install pathlib
-pip3 install wget
-pip3 install getch
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python
+sudo pip3 install py-getch 
+sudo pip3 install pathlib
+sudo pip3 install wget
+sudo pip3 install getch
+
 
  
