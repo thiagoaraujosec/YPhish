4c0c69bc509cd9cfad2aa6c21efadba1c95ca677
thiagous06 <64616891+thiagous06@users.noreply.github.com>
2021-06-11 17:44:38 -0300

    Correción de errores

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ mkdir -p tools/Reconocimiento
 mkdir -p tools/Spoofing
 sudo apt-get install python3
 sudo apt-get install python3-pip
-sudo apt-get install python
 sudo pip3 install py-getch 
 sudo pip3 install pathlib
 sudo pip3 install wget
