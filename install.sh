4e81084580687bfe36aeb9dac5300fbec4e33fa1
Thiagous <64616891+thiagous06@users.noreply.github.com>
2021-09-11 14:54:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ mkdir -p tools/Reconocimiento
 mkdir -p tools/Spoofing
 sudo apt-get install python3
 sudo apt-get install python3-pip
+sudo apt-get install php
 sudo pip3 install pathlib
 sudo pip3 install wget 
 sudo pip install pathlib
